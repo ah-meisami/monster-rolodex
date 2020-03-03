46fed0637f5d1fbdface261f51d2dac95e7de164
Meisami Amir Hossein <Ah_Meisami@iranet.net>
2020-03-03 09:53:18 +0330

    Updates

---
+++ precache-manifest.53e748cb8db3cdd152becd824b9f13f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d76e9f3e34d4f863aad467d0af50dba4",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "590f39844718b8f39482",
+    "url": "/monster-rolodex/static/css/main.6459d069.chunk.css"
+  },
+  {
+    "revision": "50f8a2ec6c2b2cdc51f1",
+    "url": "/monster-rolodex/static/js/2.720910a5.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monster-rolodex/static/js/2.720910a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "590f39844718b8f39482",
+    "url": "/monster-rolodex/static/js/main.1fc3bd50.chunk.js"
+  },
+  {
+    "revision": "2b407a4fa0ef2084fdc1",
+    "url": "/monster-rolodex/static/js/runtime-main.0c33d4be.js"
+  }
+]);
